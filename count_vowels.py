62bee7323e8b8d59dc37aa3d91c6011b76d2b29d
Reuven Lerner <reuven@lerner.co.il>
2025-05-06 19:34:30 +0300

    gitautosync update 22

---
+++ count_vowels.py
@@ -0,0 +1,5 @@
+# I'm writing a function that, given a string, returns a dict 
+# whose keys are the vowels (a, e, i, o, u) and whose values are
+# integers, the number of times that each vowel appeared in 
+# the string
+
