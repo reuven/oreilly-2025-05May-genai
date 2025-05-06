c3320ae584eab7546cc8290cd5b8603a9212ca9b
Reuven Lerner <reuven@lerner.co.il>
2025-05-06 19:57:36 +0300

    gitautosync update 31

--- piglatin.py
+++ piglatin.py
@@ -6,4 +6,19 @@ def pl_word(text):
 
 for one_word in ['computer', 'apple', 'papaya', 'elephant']:
     print(f'{one_word}: {pl_word(one_word)}')
+
+
+def pl_sentence(text):
+    output = []
+
+    for one_word in text.split():
+        output.append(pl_word(one_word))
+
+    if text[0] in 'aeiou':
+        return text + 'way'
+    else:
+        return text[1:] + text[0] + 'ay'
+
+for one_word in ['computer', 'apple', 'papaya', 'elephant']:
+    print(f'{one_word}: {pl_word(one_word)}')
     
